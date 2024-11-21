57eed4f7fea7f5dbd6e23ace6821f9cec98da19c
rolandd <rolandd@berea.edu>
2024-11-21 01:45:20 -0500

    Stubbed out password_manager.py and user_account.py

---
+++ Safepass/password_manager.py
@@ -0,0 +1,18 @@
+class PasswordManager:
+    def __init__(self):
+        pass
+
+    def add_password(self, site, password):
+        pass
+
+    def remove_password(self, site):
+        pass
+
+    def categorize_password(self, site, category):
+        pass
+
+    def get_passwords_by_category(self, category):
+        pass
+
+    def list_all_passwords(self):
+        pass
---
+++ Safepass/user_account_class.py
@@ -0,0 +1,15 @@
+class UserAccount:
+    def __init__(self, username, password):
+        pass
+
+    def encrypt_password(self, password):
+        pass
+
+    def decrypt_password(self, encrypted_password):
+        pass
+
+    def save_password(self, site, password):
+        pass
+
+    def retrieve_password(self, site):
+        pass
